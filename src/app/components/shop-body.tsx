30a472ebd4b9923852e364ebf60b7fc423453550
CollinDex <obetscollins@gmail.com>
2024-07-06 19:06:22 +0100

    Completed Left Category Nav

--- src/app/components/shop-body.tsx
+++ src/app/components/shop-body.tsx
@@ -1,7 +1,21 @@
 const ShopBody = () => {
+    const links = ['Water Bottles', 'Tot Bags', 'Crocs', 'Bikers Shorts', 'Crop Tops', 'Bucket Hats', 'Jewelry Box'];
     return (
-        <div>
-            Shop Body
+        <div className="w-full h-full grid grid-cols-[1fr_4fr] gap-4 p-8">
+            <div>
+                <p className="text-xl pb-4">Category</p>
+                {
+                    links.map((link, index) => {
+                        return (
+                            <p className="text-sm pb-2 hover:cursor-pointer" key={index}>{link}</p>
+                        ) 
+                    })
+                }
+            </div>
+            <div className="">
+                <div>Search Bar</div>
+                <div>Items Conatiner</div>
+            </div>
         </div>
     );
 };
