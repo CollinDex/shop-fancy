0629b00a5033237ef2e4b46e1db275ed532ed362
CollinDex <obetscollins@gmail.com>
2024-07-08 07:54:56 +0100

    Changed Icons

--- src/app/components/new-arrival.tsx
+++ src/app/components/new-arrival.tsx
@@ -11,12 +11,8 @@ const popins = Poppins({
 const NewArrival:React.FC = () => {
     return (
         <div className={`${popins.className} w-full`}>
-            <div className="flex flex-row text-white w-full bg-headerBg justify-between p-2 md:p-4 items-center shadow">
+            <div className="flex flex-row text-white w-full bg-headerBg justify-start p-2 md:p-4 items-center shadow">
             <p className=" text-2xl md:text-4xl">New Arrival</p>
-            <div>
-                <p>View All</p>
-                <div>Arrow</div>
-            </div>
             </div>
             <ItemsContainer/>
         </div>
